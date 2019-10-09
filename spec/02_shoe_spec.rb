991b504d7cd12a6f3ab17016c5ea93daaaceae5c
Valeria <valeria.gutsuliak@gmail.com>
2019-10-09 17:11:48 -0500

    Done.

--- spec/02_shoe_spec.rb
+++ spec/02_shoe_spec.rb
@@ -1,3 +1,27 @@
+class Shoe 
+  attr_accessor :brand, :color, :size, :material, :condition
+
+def initialize(brand)
+  @brand = brand 
+end
+
+def color=(color)
+  @color = color
+end 
+def size=(size)
+  @size = size
+end
+def material=(material)
+  @material = material
+end
+def condition=(condition)
+  @condition = condition
+end 
+def cobble
+  puts "Your shoe is as good as new!"
+  @condition = "new"
+end
+end 
 describe 'Shoe' do
   let(:shoe) { Shoe.new("Nike") }
 
